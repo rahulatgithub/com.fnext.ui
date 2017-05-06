29a5930a8fd5c6e4194789ee7da857a3cdd635ea
rahulatgithub <shri.rahul1990@gmail.com>
2017-05-06 11:48:41 +0530

    Some clean up

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -8,7 +8,7 @@
  */
 function MainCtrl() {
 
-    this.userName = 'Example user';
+    this.userName = 'Jayant Bhagat';
     this.helloText = 'Welcome in SeedProject';
     this.descriptionText = 'It is an application skeleton for a typical AngularJS web app. You can use it to quickly bootstrap your angular webapp projects and dev environment for these projects.';
 
